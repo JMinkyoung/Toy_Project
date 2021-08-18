22864de54acd623d78afbcbfd0fd39c12e1ec2f7
JMinkyoung <whk5817@gmail.com>
2021-08-18 17:59:36 +0900

    Index, DiaryList 분리

---
+++ Drawing Diary/pages/DiaryList.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import MonthNavi from '../components/MonthNavi';
+import DiaryDisplay from '../components/DiaryDisplay';
+import { useSelector} from 'react-redux';
+
+const DiaryList = () => {
+    const diarys = useSelector((state)=>state.diaryReducer.diarys);
+    // 현재 날짜를 기준으로 보여준다.
+    const today = new Date();
+    const [month, setMonth] = useState("0"+ (today.getMonth() + 1));
+    let year = today.getFullYear();
+    
+    return(
+        <>  
+            <MonthNavi setMonth={setMonth} />
+            {diarys.map((diary)=>{
+                if(diary.date[1] === month){
+                    return <DiaryDisplay key={diary.date} data={diary}/>
+                }
+            })}
+        </>
+    );
+};
+
+export default DiaryList;
--- Drawing Diary/pages/index.js
+++ Drawing Diary/pages/index.js
@@ -1,26 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
-import DiaryDisplay from '../components/DiaryDisplay';
-import MonthNavi from '../components/MonthNavi';
-import { useSelector} from 'react-redux';
 
 const Index = () => {
-    const diarys = useSelector((state)=>state.diaryReducer.diarys);
-    // 현재 날짜를 기준으로 보여준다.
-    const today = new Date();
-    const [month, setMonth] = useState("0"+ (today.getMonth() + 1));
-    let year = today.getFullYear();
-    
-    const Router = useRouter();
+    const router = useRouter();
     return(
         <>  
-            {/* <button onClick={()=>Router.push('/NewDiary')}>일기 작성</button> */}
-            <MonthNavi setMonth={setMonth} />
-            {diarys.map((diary)=>{
-                if(diary.date[1] === month){
-                    return <DiaryDisplay key={diary.date} data={diary}/>
-                }
-            })}
+            <h1>사이트 소개 및 이동버튼 여기에</h1>
+            <button onClick={()=>router.push('/NewDiary')}>일기 작성</button>
+            <button onClick={()=>router.push('/DiaryList')}>일기 보기</button>
         </>
     );
 };
