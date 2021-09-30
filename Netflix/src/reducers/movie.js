2ee1813b67570975713686a74d6b372ce2b6864e
JMinkyoung <whk5817@gmail.com>
2021-09-30 20:03:13 +0900

    Movie Data API 연동

--- Netflix/src/reducers/movie.js
+++ Netflix/src/reducers/movie.js
@@ -1,11 +1,56 @@
 export const initialState = {
-  movies: [],
+  infos:[],
+  videos: [],
+  keywords: [],
+  credits:[],
+  similars: [],
+
+  popularmovie: [],
+
+  getInfoLoading: false,
+  getInfoDone: false,
+  getInfoError: null,
+
+  getVideoLoading: false,
+  getVideoDone: false,
+  getVideoError: null,
+
+  getKeywordLoading: false,
+  getKeywordDone: false,
+  getKeywordError: null,
+
+  getCreditLoading: false,
+  getCreditDone: false,
+  getCreditError: null,
+
+  getSimilarLoading: false,
+  getSimilarDone: false,
+  getSimilarError: null,
 
   getPopularLoading: false,
   getPopularDone: false,
   getPopularError: null,
 }
 
+export const GET_MOVIE_INFO_REQUEST = 'GET_MOVIE_INFO_REQUEST';
+export const GET_MOVIE_INFO_SUCCESS = 'GET_MOVIE_INFO_SUCCESS';
+export const GET_MOVIE_INFO_FAILURE = 'GET_MOVIE_INFO_FAILURE';
+
+export const GET_MOVIE_VIDEO_REQUEST = 'GET_MOVIE_VIDEO_REQUEST';
+export const GET_MOVIE_VIDEO_SUCCESS = 'GET_MOVIE_VIDEO_SUCCESS';
+export const GET_MOVIE_VIDEO_FAILURE = 'GET_MOVIE_VIDEO_FAILURE';
+
+export const GET_MOVIE_KEYWORDS_REQUEST = 'GET_MOVIE_KEYWORDS_REQUEST';
+export const GET_MOVIE_KEYWORDS_SUCCESS = 'GET_MOVIE_KEYWORDS_SUCCESS';
+export const GET_MOVIE_KEYWORDS_FAILURE = 'GET_MOVIE_KEYWORDS_FAILURE';
+
+export const GET_MOVIE_CREDITS_REQUEST = 'GET_MOVIE_CREDITS_REQUEST';
+export const GET_MOVIE_CREDITS_SUCCESS = 'GET_MOVIE_CREDITS_SUCCESS';
+export const GET_MOVIE_CREDITS_FAILURE = 'GET_MOVIE_CREDITS_FAILURE';
+
+export const GET_MOVIE_SIMILAR_REQUEST = 'GET_MOVIE_SIMILAR_REQUEST';
+export const GET_MOVIE_SIMILAR_SUCCESS = 'GET_MOVIE_SIMILAR_SUCCESS';
+export const GET_MOVIE_SIMILAR_FAILURE = 'GET_MOVIE_SIMILAR_FAILURE';
 
 export const GET_POPULAR_MOVIE_REQUEST = 'GET_POPULAR_MOVIE_REQUEST';
 export const GET_POPULAR_MOVIE_SUCCESS = 'GET_POPULAR_MOVIE_SUCCESS';
@@ -14,6 +59,106 @@ export const GET_POPULAR_MOVIE_FAILURE = 'GET_POPULAR_MOVIE_FAILURE';
 
 const reducer = (state = [] , action) => {
     switch(action.type){
+      case GET_MOVIE_INFO_REQUEST:
+        return{
+            ...state,
+            getInfoLoading:true,
+            getInfoDone:false,
+            getInfoError:null,
+        };
+      case GET_MOVIE_INFO_SUCCESS:
+        return{
+            ...state,
+            infos: action.data,
+            getInfoLoading:false,
+            getInfoDone:true,
+        };
+      case GET_MOVIE_INFO_FAILURE:
+        return{
+          ...state,
+          getInfoLoading:false,
+          getInfoError:action.error,
+        };
+      case GET_MOVIE_VIDEO_REQUEST:
+        return{
+            ...state,
+            getVideoLoading:true,
+            getVideoDone:false,
+            getVideoError:null,
+        };
+      case GET_MOVIE_VIDEO_SUCCESS:
+        return{
+            ...state,
+            videos: action.data,
+            getVideoLoading:false,
+            getVideoDone:true,
+        };
+      case GET_MOVIE_VIDEO_FAILURE:
+        return{
+          ...state,
+          getVideoLoading:false,
+          getVideoError:action.error,
+        };
+      case GET_MOVIE_KEYWORDS_REQUEST:
+        return{
+            ...state,
+            getKeywordLoading:true,
+            getKeywordDone:false,
+            getKeywordError:null,
+        };
+      case GET_MOVIE_KEYWORDS_SUCCESS:
+        return{
+            ...state,
+            keywords: action.data,
+            getKeywordLoading:false,
+            getKeywordDone:true,
+        };
+      case GET_MOVIE_KEYWORDS_FAILURE:
+        return{
+          ...state,
+          getKeywordLoading:false,
+          getKeywordError:action.error,
+        };
+      case GET_MOVIE_CREDITS_REQUEST:
+        return{
+            ...state,
+            getCreditLoading:true,
+            getCreditDone:false,
+            getCreditError:null,
+        };
+      case GET_MOVIE_CREDITS_SUCCESS:
+        return{
+            ...state,
+            credits: action.data,
+            getCreditLoading:false,
+            getCreditDone:true,
+        };
+      case GET_MOVIE_CREDITS_FAILURE:
+        return{
+          ...state,
+          getCreditLoading:false,
+          getCreditError:action.error,
+        };
+      case GET_MOVIE_SIMILAR_REQUEST:
+        return{
+            ...state,
+            getSimilarLoading:true,
+            getSimilarDone:false,
+            getSimilarError:null,
+        };
+      case GET_MOVIE_SIMILAR_SUCCESS:
+        return{
+            ...state,
+            similars: action.data,
+            getSimilarLoading:false,
+            getSimilarDone:true,
+        };
+      case GET_MOVIE_SIMILAR_FAILURE:
+        return{
+          ...state,
+          getSimilarLoading:false,
+          getSimilarError:action.error,
+        };
       case GET_POPULAR_MOVIE_REQUEST:
         return{
             ...state,
@@ -24,7 +169,7 @@ const reducer = (state = [] , action) => {
       case GET_POPULAR_MOVIE_SUCCESS:
         return{
             ...state,
-            movies: action.data,
+            popularmovie: action.data,
             getPopularLoading:false,
             getPopularDone:true,
         };
--- Netflix/src/sagas/movie.js
+++ Netflix/src/sagas/movie.js
@@ -1,10 +1,119 @@
 import { all, fork ,put, takeLatest, call} from "redux-saga/effects";
 import axios from 'axios';
-import { GET_POPULAR_MOVIE_FAILURE, GET_POPULAR_MOVIE_REQUEST, GET_POPULAR_MOVIE_SUCCESS } from "../reducers/movie";
+import { 
+    GET_MOVIE_INFO_REQUEST, GET_MOVIE_INFO_SUCCESS, GET_MOVIE_INFO_FAILURE,
+    GET_MOVIE_VIDEO_REQUEST, GET_MOVIE_VIDEO_SUCCESS, GET_MOVIE_VIDEO_FAILURE,
+    GET_MOVIE_KEYWORDS_REQUEST, GET_MOVIE_KEYWORDS_SUCCESS, GET_MOVIE_KEYWORDS_FAILURE,
+    GET_MOVIE_CREDITS_REQUEST, GET_MOVIE_CREDITS_SUCCESS, GET_MOVIE_CREDITS_FAILURE,
+    GET_MOVIE_SIMILAR_REQUEST, GET_MOVIE_SIMILAR_SUCCESS, GET_MOVIE_SIMILAR_FAILURE,
 
 
+    GET_POPULAR_MOVIE_FAILURE, GET_POPULAR_MOVIE_REQUEST, GET_POPULAR_MOVIE_SUCCESS } from "../reducers/movie";
+
+
+
+function movieinfoAPI(data) {
+    return axios.get(`https://api.themoviedb.org/3/movie/${data}?api_key=0f54240da860f0ee0a59657346e7a8cc&language=ko-KR`);
+}
+
+function movievideoAPI(data) {
+    return axios.get(`https://api.themoviedb.org/3/movie/${data}/videos?api_key=0f54240da860f0ee0a59657346e7a8cc&language=ko-KR`);
+}
+
+function moviekeywordAPI(data) {
+    return axios.get(`https://api.themoviedb.org/3/movie/${data}/keywords?api_key=0f54240da860f0ee0a59657346e7a8cc`);
+}
+
+function moviecreditAPI(data) {
+    return axios.get(`https://api.themoviedb.org/3/movie/${data}/credits?api_key=0f54240da860f0ee0a59657346e7a8cc&language=ko-KR`);
+}
+
+function moviesimilarAPI(data) {
+    return axios.get(`https://api.themoviedb.org/3/movie/${data}/similar?api_key=0f54240da860f0ee0a59657346e7a8cc&language=ko-KR&page=1`);
+}
+    
 function popularmovieAPI() {
     return axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=0f54240da860f0ee0a59657346e7a8cc&language=ko-KR&page=1&region=KR`);
+}
+
+function* getMOVIEInfo(action) {
+    try {
+        const result = yield call(movieinfoAPI,action.data);
+        yield put({
+            type: GET_MOVIE_INFO_SUCCESS,
+            data: result.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: GET_MOVIE_INFO_FAILURE,
+            error: error.response.data
+        });
+    }
+}
+
+function* getMOVIEVideo(action) {
+    try {
+        const result = yield call(movievideoAPI,action.data);
+        yield put({
+            type: GET_MOVIE_VIDEO_SUCCESS,
+            data: result.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: GET_MOVIE_VIDEO_FAILURE,
+            error: error.response.data
+        });
+    }
+}
+
+function* getMOVIEKeyword(action) {
+    try {
+        const result = yield call(moviekeywordAPI,action.data);
+        yield put({
+            type: GET_MOVIE_KEYWORDS_SUCCESS,
+            data: result.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: GET_MOVIE_KEYWORDS_FAILURE,
+            error: error.response.data
+        });
+    }
+}
+
+function* getMOVIECredit(action) {
+    try {
+        const result = yield call(moviecreditAPI,action.data);
+        yield put({
+            type: GET_MOVIE_CREDITS_SUCCESS,
+            data: result.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: GET_MOVIE_CREDITS_FAILURE,
+            error: error.response.data
+        });
+    }
+}
+
+function* getMOVIESimilar(action) {
+    try {
+        const result = yield call(moviesimilarAPI,action.data);
+        yield put({
+            type: GET_MOVIE_SIMILAR_SUCCESS,
+            data: result.data,
+        });
+    } catch (error) {
+        console.error(error);
+        yield put({
+            type: GET_MOVIE_SIMILAR_FAILURE,
+            error: error.response.data
+        });
+    }
 }
 
 function* getPopularMoive(action) {
@@ -23,12 +132,37 @@ function* getPopularMoive(action) {
     }
 }
 
+function* watchGetMovieInfo() {
+    yield takeLatest(GET_MOVIE_INFO_REQUEST,getMOVIEInfo);
+}
+
+function* watchGetMovieVideo() {
+    yield takeLatest(GET_MOVIE_VIDEO_REQUEST,getMOVIEVideo);
+}
+
+function* watchGetMovieKeyword() {
+    yield takeLatest(GET_MOVIE_KEYWORDS_REQUEST,getMOVIEKeyword);
+}
+
+function* watchGetMovieCredit() {
+    yield takeLatest(GET_MOVIE_CREDITS_REQUEST,getMOVIECredit);
+}
+
+function* watchGetMovieSimilar() {
+    yield takeLatest(GET_MOVIE_SIMILAR_REQUEST,getMOVIESimilar);
+}
+
 function* watchPopularMovie() {
     yield takeLatest(GET_POPULAR_MOVIE_REQUEST,getPopularMoive);
 }
 
 export default function* movieSaga() {
     yield all([
+        fork(watchGetMovieInfo),
+        fork(watchGetMovieVideo),
+        fork(watchGetMovieKeyword),
+        fork(watchGetMovieCredit),
+        fork(watchGetMovieSimilar),
         fork(watchPopularMovie),
     ]);
 }
